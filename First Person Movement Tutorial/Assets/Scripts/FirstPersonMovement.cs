be1343d8dc72c192fcb17744e98573eb3481acf1
Seth <sethcram@gmail.com>
2022-03-21 22:06:47 -0700

    Unity Project Commit

---
+++ First Person Movement Tutorial/Assets/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    private CharacterController controller;
+
+    //different speeds:
+    public float walkSpeed = 5f;
+    public float runSpeed = 12f;
+    private float playerSpeed;
+
+    //gravity application:
+    private float verticalVelocity; //we only use its y-coord
+    public float gravity = -9.81f; //default gravity in m/s (changing to -20f usually feels better)
+
+    //to check if grounded:
+    public Transform groundCheck; //for checking if grounded
+    public float groundSphereRadius = 0.4f; //sphere drawn around 'groundCheck' obj
+    public LayerMask groundMask; //layers sphere is checking for
+    private bool isGrounded;
+
+    //jumping varibles:
+    public float jumpHeight = 3f; //height want player to be able to jump
+    public float midAirSlopeLimit = 90;
+    public float midAirStepOffset = 0.1f;
+
+    //grounded resets for w/ jumping:
+    public float groundedSlopeLimit = 45;
+    public float groundedStepOffset = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //applies this downward velocity to make sure player on ground:
+        float groundedDownwardVelocity = -2f; 
+
+        //set if currently grounded:
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundSphereRadius, groundMask);
+
+        //reset y velocity, slope limit, and step offset if player grounded and he wasnt before:
+        if(isGrounded && verticalVelocity < 0)
+        {
+            //reset slope limit and step offset w/ land:
+            controller.slopeLimit = groundedSlopeLimit;
+            controller.stepOffset = groundedStepOffset;
+
+            verticalVelocity = groundedDownwardVelocity; //not zero just incase player not totally on the grnd yet, this will force him to the grnd
+        }
+
+        //get input:
+        float xInput = Input.GetAxisRaw("Horizontal"); //use 'Raw' instead so player stops instantly instead of sliding to a stop
+        float zInput = Input.GetAxisRaw("Vertical");
+
+        //set 'playerSpeed' to 'runSpeed' if grounded and left shift held down, otherwise set it to 'walkSpeed' if grounded:
+        if(isGrounded)
+        {
+            if(Input.GetButton("Fire3")) //'Fire3' = axis for left shift, (mouse 2?), and controller something
+            {
+                playerSpeed = runSpeed;
+            }
+            else
+            {
+                playerSpeed = walkSpeed;
+            }
+        }
+
+        //turn input into dir want to move relative to curr rot:
+        //Vector3 direction = new Vector3(xInput, 0f, zInput).normalized; //dont want bc moves using global coords, so moves same way independent of player's already set rot
+        Vector3 moveDirection = transform.right * xInput + transform.forward * zInput; //'.right' and '.forward' use obj's curr local coord syst (don't need to normalize?)
+
+        //normalize move direction vector so moving diagonal not faster than in 1 dir:
+        moveDirection = moveDirection.normalized;
+
+        //apply player speed:
+        //moveDirection *= playerSpeed;
+
+        //move controller in desired direction:
+        controller.Move(moveDirection * playerSpeed * Time.deltaTime);
+
+        //jump and change slope limit, step offset if currently grounded and pressed the button:
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            //increase slope limit to avoid jitter w/ mid-air
+            controller.slopeLimit = midAirSlopeLimit;
+
+            //lower step offset so dont land on surfaces higher than feet:
+            controller.stepOffset = midAirStepOffset;
+
+            verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity); // v = sqrt(h * -2 * g) is the velocity req'd to jump certain height
+        }
+
+        //apply gravity every second in freefall:
+        verticalVelocity += gravity * Time.deltaTime;
+        controller.Move( new Vector3(0, verticalVelocity * Time.deltaTime, 0));
+
+        //combine move direction and y-velocity to move controller:
+        //velocity = velocity + moveDirection;
+        //controller.Move(velocity * Time.deltaTime);
+
+        //reset y-coord velocity if controller collides w/ something above it (need to be below all 'Move()' calls bc they ret collision flags):
+        if ((controller.collisionFlags & CollisionFlags.Above) != 0)
+        {
+            verticalVelocity = groundedDownwardVelocity;
+
+            //check: Debug.Log("Vertical velocity reset bc of head collision");
+        }
+    }
+
+    //draw black wire sphere around 'groundCheck' obj when player obj is selected:
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(groundCheck.position, groundSphereRadius);
+    }
+}
---
+++ First Person Movement Tutorial/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseMoveSpeed = 10f;
+
+    private Transform player;
+
+    private float xAxisRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = gameObject.GetComponentInParent<Transform>(); //doesnt work bc fills space with this obj's transform since its first
+
+        player = transform.parent.GetComponent<Transform>();
+
+        //hide and lock cursor to center of screen:
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set mouse input based on mouse:
+        float mouseXinput = Input.GetAxis("Mouse X");
+        float mouseYinput = Input.GetAxis("Mouse Y");
+
+        //apply mouse sensitivity:
+        mouseXinput *= mouseMoveSpeed;
+        mouseYinput *= mouseMoveSpeed;
+
+        //apply input independent of framerate (once everytime 'Update()' called) (don't need to mult by Time.deltaTime bc mouse input is already framerate independent)
+
+        //decrease x axis's rotation by however much move mouse (needs to be - and not + for some reason):
+        xAxisRotation -= mouseYinput;
+
+        //only rotate cam up/down from feet to sky (cant over rotate and look behind player): 
+        xAxisRotation = Mathf.Clamp(xAxisRotation, -90f, 90f);
+
+        //rot camera by new x axis rot:
+        transform.localRotation = Quaternion.Euler(xAxisRotation, 0f, 0f);
+
+        //set player rotation based on mouse horizontal movement (dont use eulers?)
+        player.Rotate(Vector3.up * mouseXinput); //roting around y-axis (so Vector3.up)
+    }
+}
